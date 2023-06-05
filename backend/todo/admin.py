d1a3d567fdf517c7c94528f74c10555aff83030f
Lauren Emick <laurenemick@Laurens-MacBook-Pro.local>
2023-06-05 08:57:49 -0700

    migration

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -4,7 +4,6 @@ from .models import Todo
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
-# Register your models here.
 
 admin.site.register(Todo, TodoAdmin)
 
