0d96b893a7c72d19a227d5d8af3c89bc2f98a8f1
Lauren Emick <laurenemick@Laurens-MacBook-Pro.local>
2023-06-05 10:02:59 -0700

    display mock data

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Task from './Task';
+import { fetchTasks } from '../store/actions';
+import taskData from '../assets/mockData.json';
 
 const TaskList = (props) => {
-    // const { tasks } = props.tasks
+    const dispatch = useDispatch();
+    const { tasks, isLoading, isError, errorMsg } = useSelector(
+        state => state
+    );
 
-    // return (
-    //     <div>
-    //         {!tasks ? (
-    //             <div />
-    //         ) : (
-    //             tasks.map(task => (
-    //                 <Task key={task.id} task={task} />
-    //             ))
-    //         )}
-    //     </div>
-    // )
+    useEffect(() => {
+        dispatch(fetchTasks(taskData));
+    }, []);
+
+    return (
+        <div>
+            {isLoading && <div>Data loading...</div>}
+            {isError && <div>Error loading data: {errorMsg}</div>}
+            {!isLoading && !isError && (
+                tasks.map(task => (
+                    <Task key={task.id} task={task} />
+                ))
+            )}
+        </div>
+    )
 }
 
 export default TaskList;
